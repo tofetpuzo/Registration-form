c70e2b16f6e72f46947d08d80e757e8c6ee36c06
tofetpuzo <topetemiola09@gmail.com>
2022-02-11 10:45:44 +0000

    building-reg-form

---
+++ database.js
@@ -0,0 +1,25 @@
+const mysql_db = require('mysql');
+
+const pool = mysql_db.createPool({
+    host: 'localhost',
+    datababse: 'testapi',
+    user: 'root',
+    password: 'Mariopuzo4u.',
+    connectionLimit: 5
+});
+
+pool.getConnection((error, connection) => {
+    if (error) {
+        if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('Database Connection Lost');
+        }
+        if (error.code === 'ER_CON_COUNT_ERROR') {
+            console.log('Database has too many connection');
+        }
+        if (error.code === 'ECONNREFUSED') {
+            console.log('Database connection was refused');
+        }
+    } if (connection) connection.release();
+    return;
+});
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 var express = require('express');
 var app = express();
+
+//This creates a pool for connection
+const pool = require("./database")
+
+//This helps you get information from the database passed 
