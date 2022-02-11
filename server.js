1e555ce29216811a64de0456387ff8a8d3541865
tofetpuzo <topetemiola09@gmail.com>
2022-02-11 10:53:21 +0000

    building-reg-form

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 var app = express();
 
 //This creates a pool for connection
-const pool = require("./database")
+const pool = require("./database");
 
 //This helps you get information from the database passed 
+const{parse} = require("querystring");
+
+//This helps read the html file
+var fs = require("fs");
+app.get("/reg", function(req. res){
+    //This code is to read data from file
+    fs.readFile("./reg_form.html", "utf8", function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(data);
+    });
+});
